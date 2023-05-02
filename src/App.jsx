c88b9d16cf5740e17ab60f4e354ace7a3a808943
Edinei Pinho dos Santos <edneyrock@gmail.com>
2023-05-01 22:14:52 -0300

    :sparkles: feat: Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 // import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Api from './api/Api.jsx';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
 
 const App = () => {
   return <div>
-    <Api />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </div>;
 };
 
--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,5 +1,7 @@
 import UserPost from "./endpoints/UserPost"
 import TokenPost from "./endpoints/TokenPost"
+import PhotoPost from "./endpoints/PhotoPost"
+import PhotoGet from "./endpoints/PhotoGet"
 
 const Api = () => {
   return (
@@ -8,6 +10,10 @@ const Api = () => {
       <UserPost />
       <h2>TOKEN POST</h2>
       <TokenPost />
+      <h2>PHOTO POST</h2>
+      <PhotoPost />
+      <h2>PHOTO GET</h2>
+      <PhotoGet />
     </div>
   )
 }
