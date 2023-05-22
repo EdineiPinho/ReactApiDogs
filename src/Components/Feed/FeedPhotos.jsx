5a153a15576cdd7b26c88af39a8b6a7130dd4261
Edinei Pinho dos Santos <edneyrock@gmail.com>
2023-05-22 01:04:34 -0300

    :sparkles: feat: PhotoItens

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -7,7 +7,7 @@ import Loading from '../Helper/Loading'
 import styles from './FeedPhotos.module.css'
 
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
@@ -24,7 +24,12 @@ const FeedPhotos = () => {
   if (data) {
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        {data.map((photo) => (<FeedPhotoItem key={photo.id} photo={photo} />))}
+        {data.map((photo) => (
+          <FeedPhotoItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto} />
+        ))}
 
       </ul>
     )
