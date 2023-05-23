bbcd1c32e9b116e7e7dcfa5fc93ad27ff36fbd94
edineipinho <edneyrock@gmail.com>
2023-05-23 10:29:12 -0300

    :sparkles: feat(Loading): Loading is working

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
+import Loading from './Components/Helper/Loading';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    {/* <Loading /> */}
+  </>
+);
